55c0dde5652473ec4f9ebc9ceca81312f34b6546
viktorija6420 <viktorija.gjorgjievska@gmail.com>
2017-08-10 20:33:58 -0400

    finished exercise 0 and 1

---
+++ exercises.rb
@@ -0,0 +1,17 @@
+fav_colors = ["orange", "green", "yellow"]
+age = [27, 33, 25, 44]
+coin_flip = ["heads", "tails", "heads", "tails", "heads"]
+favourite_artists = ["Claire Danes", "Kevin Spacey", "Casey Affleck"]
+fav_colors1 = [:orange, :green, :yellow]
+
+words_and_meaning = { :bright => "shining", :matrix => "fine_material", :hyperbole => "exaggeration" }
+fav_movie_and_year = { :Forrest_Gump => 1994, :Moonlight => 2016, :The_Martian => 2015 }
+cities_and_population = { :Montreal => 1649519, :Skopje => 1000000, :Mexico_City => 8918653 }
+names = { :Anna => 27, :Maria => 25, :Brian => 33, :Martin => 44}
+
+print coin_flip
+print fav_colors[0]
+print age.sort
+
+age << 0
+print fav_movie_and_year[:Forrest_Gump]
